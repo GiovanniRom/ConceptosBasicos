1645e38b7cbbca2a342c00fe489c3ed23be0cb67
Giovanni Romero <garm9609@gmail.com>
2021-03-20 00:59:23 -0600

    Se agregaron los objetos de Acura y Mustang

--- conceptosbasicos/ConceptosBasicos.java
+++ conceptosbasicos/ConceptosBasicos.java
@@ -25,6 +25,18 @@ public class ConceptosBasicos {
         miBocho.setModelo(1970);
         miBocho.setColor(Color.blue);
         
+        Automovil miAcura = new Automovil();
+        miAcura.setMarca("Acura");
+        miAcura.setSubMarca("Luxury");
+        miAcura.setModelo(2017);
+        miAcura.setColor(Color.gray);
+        
+        Automovil miMustang = new Automovil();
+        miMustang.setMarca("Ford");
+        miMustang.setSubMarca("Deportivo");
+        miMustang.setModelo(2015);
+        miMustang.setColor(Color.yellow);
+        
     }
     
 }
